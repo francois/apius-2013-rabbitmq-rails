f17ecda0aebffe2d2a62b01d509ae7316d06163e
François Beausoleil <francois@teksol.info>
2013-07-20 06:32:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apius::Application.config.secret_token = 'dac7ef3457c62c6a24d372a2ae77af84fc58cd77b087e57dbd5f33e5f0b7276f993a0ff0f6d3db1aeadd08478e17637cb835c96c6739951b7699d3fc6aa95974'
